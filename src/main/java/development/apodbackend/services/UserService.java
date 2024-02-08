74a137763053e5ec77f5c012282f4223f9325e2a
Lebackrobot <lebackman18@gmail.com>
2024-02-08 16:24:41 -0300

    ✨ feat(user): crud user

--- src/main/java/development/apodbackend/services/UserService.java
+++ src/main/java/development/apodbackend/services/UserService.java
@@ -1,6 +1,7 @@
 package development.apodbackend.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,7 +14,23 @@ public class UserService {
     @Autowired
     private UserRepository repository;
 
-    public List<UserModel> getAll() {
+    public List<UserModel> get() {
         return repository.findAll();
+    }
+
+    public Optional<UserModel> getById(int id) {
+        return repository.findById(id);
+    }
+
+    public UserModel createUser(UserModel user) {
+        return repository.save(user);
+    }
+
+    public UserModel getByEmail(String email) {
+        return repository.findOneByEmail(email);
+    }
+    
+    public void deleteById(int id) {
+        repository.deleteById(id);
     }
 }
