e89f67f53e425ea53d0eeea2daae8ed90fbe8dd4
Lebackrobot <lebackman18@gmail.com>
2024-02-14 01:39:48 -0300

    🔨 fix: add api_key in application.properties

--- src/main/java/development/apodbackend/apis/ApodApi.java
+++ src/main/java/development/apodbackend/apis/ApodApi.java
@@ -1,5 +1,6 @@
 package development.apodbackend.apis;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -11,6 +12,9 @@ import development.apodbackend.schemas.MailMessageSchema;
 
 @Service
 public class ApodApi {
+    @Value("${spring.apod.api.key}")
+    private String apod_api_key;
+
     private MailMessageSchema parseJsonToMailMessageSchema(String jsonString) {
         try {
             ObjectMapper objectMapper = new ObjectMapper();
@@ -33,7 +37,7 @@ public class ApodApi {
     public MailMessageSchema getAstronomyPicure() {
         RestTemplate restTemplate = new RestTemplate();
 
-        String apiUrl = "https://api.nasa.gov/planetary/apod?api_key=kTFkdGobs722y9E14AhRVU9NUHBhVMksxPpdxrhx";
+        String apiUrl = "https://api.nasa.gov/planetary/apod?" + apod_api_key;
 
 
         ResponseEntity<String> response = restTemplate.getForEntity(apiUrl, String.class);
