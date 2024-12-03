e6c80b3480c10b4dace7b7afa0d9c5b2f450e94a
Lebackrobot <lebackman18@gmail.com>
2024-12-02 23:23:48 -0300

    chore: change "SubscriptionValdationPayloadDto" to "SubscriptionConfirmationDto"

--- src/main/java/com/apod/backend/dtos/payloads/SubscriptionConfirmationPayloadDto.java
+++ src/main/java/com/apod/backend/dtos/payloads/SubscriptionConfirmationPayloadDto.java
@@ -3,7 +3,7 @@ package com.apod.backend.dtos.payloads;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
-public record SubscriptionValidationPayloadDto(
+public record SubscriptionConfirmationPayloadDto(
         @NotBlank String token,
         @Email String email,
         @NotBlank String name
