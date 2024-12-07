aa4fbff417bb7e797e51932353d89cefe539f306
Lebackrobot <lebackman18@gmail.com>
2024-12-07 20:59:32 -0300

    🐇 feat: rabbitMq configuration

---
+++ src/main/java/com/apod/backend/configs/RabbitConfig.java
@@ -0,0 +1,15 @@
+package com.apod.backend.configs;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+    @Bean
+    Queue queue() {
+        String queue = "tokens";
+        return new Queue(queue, true);
+    }
+
+}
